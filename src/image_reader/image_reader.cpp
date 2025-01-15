fa49bbdc3d5d9969242fad98f3d3aa187ca4961b
Max Gorbatenko <max@musclecomputing.com>
2025-01-14 18:36:00 -0600

    Made it more C++ish.

--- src/image_reader/image_reader.cpp
+++ src/image_reader/image_reader.cpp
@@ -5,16 +5,19 @@
 
 using namespace cv;
 
-int show_image() {
+int show_image()
+{
     std::string image_path = samples::findFile("buck.png");
     Mat img = imread(image_path, IMREAD_COLOR);
-    if(img.empty()) {
+    if(img.empty())
+    {
         std::cout << "Could not read the image: " << image_path << std::endl;
         return 1;
     }
     imshow("Display window", img);
     int key = waitKey(0); // Wait for a keystroke in the window
-    if(key == 's') {
+    if(key == 's')
+    {
         imwrite("starry_night.png", img);
     }
     return 0;
