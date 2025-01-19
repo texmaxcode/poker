a338f57526245734e0e6a1b1b8ffff0f01829b24
Max Gorbatenko <max@musclecomputing.com>
2025-01-19 12:58:09 -0600

    Fixed formatting.

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-     qmlRegisterType<ImageReader>("com.musclecomputing", 1, 0, "ImageReader");
+    qmlRegisterType<ImageReader>("com.musclecomputing", 1, 0, "ImageReader");
 
     QObject::connect(
         &engine, &QQmlApplicationEngine::objectCreationFailed,
